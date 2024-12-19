654f68b892209908a8cc7e50e11338db08953c20
Samuel Akinbile <samakinbile@gmail.com>
2024-12-19 15:32:26 +0000

    added hompage skeleton

--- emarket/settings.py
+++ emarket/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.socialaccount',
     'allauth.account',
+    'homepage',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +65,10 @@ ROOT_URLCONF = 'emarket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+             os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -97,7 +101,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/success'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'emarket.wsgi.application'
 
