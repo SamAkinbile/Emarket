3c75ccf52d58c649c5f715e75f5e30f88b27c7c8
Samuel Akinbile <samakinbile@gmail.com>
2024-12-21 22:01:43 +0000

    sorting functionality

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 # Create your views here.
@@ -25,19 +26,19 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-    
-    if request.GET:
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
-            categories = category.objects.filter(name__in=categories)
-    
+            categories = Category.objects.filter(name__in=categories)
+
     if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
@@ -45,10 +46,13 @@ def all_products(request):
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
             
+            
     current_sorting = f'{sort}_{direction}'
+    
     
     context = {
         'products': products,
