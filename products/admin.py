a7a5ddc4d9e51dc86dcdeb1fbeb8411c83f1c452
Samuel Akinbile <samakinbile@gmail.com>
2024-12-21 13:52:18 +0000

    costomize admin

--- products/admin.py
+++ products/admin.py
@@ -4,5 +4,24 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'age',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
 admin.site.register(Product)
 admin.site.register(Category)
