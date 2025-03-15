f9ff06300edcee7b58621e92b32080342278aeae
Samuel A <samakinbile@gmail.com>
2025-03-15 19:20:26 +0000

    Resubsmission correction started. Add admin panel to the contact/newsletter form

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import NewsletterSubscription, Contact
+
+@admin.register(NewsletterSubscription)
+class NewsletterSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('email', 'subscribed_at')  # Fields to display in the list view
+    search_fields = ('email',)  # Enable search by email
+    list_filter = ('subscribed_at',)  # Enable filtering by subscription date
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'submitted_at')  # Fields to display in the list view
+    search_fields = ('name', 'email', 'subject')  # Enable search by name, email, or subject
+    list_filter = ('submitted_at',)  # Enable filtering by submission date
